6faaaf1c1dad94c1f332a22a9f31df71f854ee11
Londonexmachina <london.ex.machina@gmail.com>
2022-04-12 21:30:44 -0700

    Add style

--- queries.js
+++ queries.js
@@ -18,9 +18,23 @@ h3.textContent = 'I\'m a blue h3';
 bodyContainer.appendChild(h3);
 
 const styleH3 = document.querySelector('h3').style;
-
 styleH3.setProperty('color','blue');
 
 const div = document.createElement('div');
-
+div.classList.add('div');
 bodyContainer.appendChild(div);
+
+const divContainer = document.querySelector('div');
+const h1 = document.createElement('h1');
+h1.textContent = "pls work";
+divContainer.appendChild(h1);
+
+const p2 = document.createElement('p');
+p2.textContent = "ME TOO!";
+
+divContainer.appendChild(p2);
+
+//const styleDiv = document.querySelector('div').style;
+//styleDiv.setProperty('background-color','pink');
+
+div.setAttribute("style", "background-color:pink;border:solid black 3px;");
